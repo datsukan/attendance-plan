c5a3f90036626d7d44d94841aa0089a579abdf72
datsukan <s.datsukan@gmail.com>
2025-02-28 19:57:02 +0900

    プライバシーポリシーについて追記

--- frontend/app/(auth)/signup/Note.tsx
+++ frontend/app/(auth)/signup/Note.tsx
@@ -3,6 +3,18 @@ import { LinkText } from '@/component/form/LinkText';
 export const Note = () => {
   return (
     <div className="w-full text-xs leading-6 text-gray-500">
+      <p className="font-bold text-red-500">
+        サインアップした際は
+        <a
+          href="https://tou.datsukan.me/attendance-plan-privacy-policy"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 hover:underline"
+        >
+          プライバシーポリシー
+        </a>
+        に同意したとみなします。
+      </p>
       <p>登録したメールアドレスは本サイト内での制御のみに使用されます。</p>
       <p>パスワードはハッシュ化されて保存され、他者には知られません。</p>
       <p>
